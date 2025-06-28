e04299e85d8c1abfec444fa8a520fbef186e7abf
Dawson Frakes <dawsonmfrakes@gmail.com>
2025-06-28 17:21:01 -0400

    just type the code man

--- build.py
+++
@@ -1,8 +0,0 @@
-import sys
-from c4 import find_entry, module_to_c
-import main
-
-match sys.argv[1:]:
-  case ("run",): find_entry(main)()
-  case ("compile",): print(module_to_c(main))
-  case _: print(f"usage: {sys.argv[0]} <run | compile>")
--- main.py
+++
@@ -1,9 +0,0 @@
-from c4 import *
-
-if OS == OSs.DARWIN:
-  @foreign("System", alt_name="_exit")
-  def sys_exit(exit_code: dtypes.CInt) -> dtypes.NoReturn: ...
-
-  @entry(alt_name="_start")
-  def start() -> dtypes.NoReturn:
-    sys_exit(0)
