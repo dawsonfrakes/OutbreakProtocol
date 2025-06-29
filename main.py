286963c631c5661dc10392e965aa06f8b67237fa
Dawson Frakes <dawsonmfrakes@gmail.com>
2025-06-29 15:40:38 -0400

    move into separate repo

--- main.py
+++ main.py
@@ -1,56 +1,29 @@
-from c4 import dtypes, entry, foreign, G, OSs, OS, struct
+from c4.c4 import dtypes, entry, foreign, G, OSs, OS
 
 if OS == OSs.WINDOWS:
-	# kernel32
-	HINSTANCE = dtypes.Pointer[dtypes.Opaque["HINSTANCE"]]
-	HMODULE = HINSTANCE
+	HINSTANCE = dtypes.Pointer[dtypes.Opaque[...]]
 
 	@foreign("kernel32")
-	def GetModuleHandleW(name: dtypes.Pointer[dtypes.CWChar]) -> HINSTANCE: ...
+	def GetModuleHandleW(name: dtypes.Optional[dtypes.Pointer[dtypes.CWChar]]) -> dtypes.Optional[HINSTANCE]: ...
 	@foreign("kernel32")
 	def ExitProcess(exit_code: dtypes.CUInt) -> dtypes.NoReturn: ...
 
-	# user32
-	HDC = dtypes.Pointer[dtypes.Opaque["HDC"]]
-	HWND = dtypes.Pointer[dtypes.Opaque["HWND"]]
-	HMENU = dtypes.Pointer[dtypes.Opaque["HMENU"]]
-	HICON = dtypes.Pointer[dtypes.Opaque["HICON"]]
-	HBRUSH = dtypes.Pointer[dtypes.Opaque["HBRUSH"]]
-	HCURSOR = dtypes.Pointer[dtypes.Opaque["HCURSOR"]]
-	HMONITOR = dtypes.Pointer[dtypes.Opaque["HMONITOR"]]
-	WNDPROC = dtypes.Procedure[dtypes.SSize, HWND, dtypes.CUInt, dtypes.USize, dtypes.SSize]
-	@struct
-	class WNDCLASSEXW:
-		cbSize: dtypes.CUInt
-		style: dtypes.CUInt
-		lpfnWndProc: WNDPROC
-		cbClsExtra: dtypes.CInt
-		cbWndExtra: dtypes.CInt
-		hInstance: HINSTANCE
-		hIcon: HICON
-		hCursor: HCURSOR
-		hbrBackground: HBRUSH
-		lpszMenuName: dtypes.Pointer[dtypes.CWChar]
-		lpszClassName: dtypes.Pointer[dtypes.CWChar]
-		hIconSm: HICON
+	@foreign("user32")
+	def SetProcessDPIAware() -> dtypes.CInt: ...
 
 	platform_hinstance: HINSTANCE
 
 	@entry
 	def WinMainCRTStartup() -> dtypes.NoReturn:
 		G.platform_hinstance = GetModuleHandleW(None)
-		print(G.platform_hinstance)
+
+		SetProcessDPIAware()
+
 		ExitProcess(0)
 elif OS == OSs.DARWIN:
-	STDOUT_FILENO = 1
-
-	@foreign("System")
-	def write(fd: dtypes.CInt, data: dtypes.Pointer[dtypes.Void], size: dtypes.USize) -> dtypes.SSize: ...
 	@foreign("System", alt_name="_exit")
 	def sys_exit(status: dtypes.CInt) -> dtypes.NoReturn: ...
 
 	@entry(alt_name="_start")
 	def start() -> dtypes.NoReturn:
-		hw = b"Hello, world\n"
-		write(STDOUT_FILENO, hw, len(hw))
 		sys_exit(0)
