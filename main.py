1c05dd268af8ace5e3264c05c2a4f2064ac98582
Dawson Frakes <dawsonmfrakes@gmail.com>
2025-06-29 17:26:03 -0400

    REEEEEEEEEEEE

--- main.py
+++ main.py
@@ -1,15 +1,40 @@
-from c4.c4 import dtypes, entry, foreign, G, OSs, OS
+from c4.c4 import dtypes, entry, foreign, G, OSs, OS, struct
 
 if OS == OSs.WINDOWS:
-	HINSTANCE = dtypes.Pointer[dtypes.Opaque[...]]
+	HINSTANCE = dtypes.Pointer[dtypes.Void]
 
 	@foreign("kernel32")
 	def GetModuleHandleW(name: dtypes.Optional[dtypes.Pointer[dtypes.CWChar]]) -> dtypes.Optional[HINSTANCE]: ...
 	@foreign("kernel32")
 	def ExitProcess(exit_code: dtypes.CUInt) -> dtypes.NoReturn: ...
 
+	HDC = dtypes.Pointer[dtypes.Void]
+	HWND = dtypes.Pointer[dtypes.Void]
+	HMENU = dtypes.Pointer[dtypes.Void]
+	HICON = dtypes.Pointer[dtypes.Void]
+	HBRUSH = dtypes.Pointer[dtypes.Void]
+	HCURSOR = dtypes.Pointer[dtypes.Void]
+	HMONITOR = dtypes.Pointer[dtypes.Void]
+	WNDPROC = dtypes.Procedure[[HWND, dtypes.CUInt, dtypes.USize, dtypes.SSize], dtypes.SSize]
+	@struct
+	class WNDCLASSEXW:
+		cbSize: dtypes.CUInt
+		style: dtypes.CUInt
+		lpfnWndProc: WNDPROC
+		cbClsExtra: dtypes.CInt
+		cbWndExtra: dtypes.CInt
+		hInstance: HINSTANCE
+		hIcon: HICON
+		hCursor: HCURSOR
+		hbrBackground: HBRUSH
+		lpszMenuName: dtypes.Optional[dtypes.Pointer[dtypes.CWChar]]
+		lpszClassName: dtypes.Optional[dtypes.Pointer[dtypes.CWChar]]
+		hIconSm: HICON
+
 	@foreign("user32")
 	def SetProcessDPIAware() -> dtypes.CInt: ...
+	@foreign("user32")
+	def RegisterClassExW(wndclass: dtypes.Optional[dtypes.Pointer[WNDCLASSEXW]]) -> dtypes.CUShort: ...
 
 	platform_hinstance: HINSTANCE
 
