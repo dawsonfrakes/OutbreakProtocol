f5f38fbb0cde50dcc2f9f72e7918f2339930b6e6
Dawson Frakes <dawsonmfrakes@gmail.com>
2025-06-30 00:14:21 -0400

    the furthest yet.

--- c4.py
+++ c4.py
@@ -28,12 +28,26 @@ class dtypes:
 	SSize = typing.Annotated[int, ctypes.c_ssize_t]
 	USize = typing.Annotated[int, ctypes.c_size_t]
 	Pointer = typing.Annotated[int | bytes | _Unused[_T] | None, ctypes.c_void_p]
+	class Struct:
+		def __class_getitem__(cls, items: tuple[str, list[tuple[str, type]], bool]):
+			name, fields, packed = items
+			fields = [(name, _dtype_to_ctype(dtype)) for name, dtype in fields]
+			class X(ctypes.Structure): _pack_ = int(packed); _fields_ = fields
+			X.__qualname__, X.__name__ = name
+			return typing.Annotated[X, X]
+	class Procedure:
+		def __class_getitem__(cls, items: tuple[type, ...]):
+			X = ctypes.CFUNCTYPE(*map(_dtype_to_ctype, items))
+			return typing.Annotated[X, X]
 
 def _dtype_to_ctype(dtype: type) -> typing.Any: return dtype if isinstance(dtype, ctypes.Structure) else getattr(dtype, "__metadata__")[0]
 def _ast_of(f: typing.Callable[..., typing.Any]) -> ast.FunctionDef: return typing.cast(ast.FunctionDef, ast.parse(textwrap.dedent(inspect.getsource(f))).body[0])
 def _dtype_of(expr: ast.expr | None, module: types.ModuleType) -> type: assert expr is not None; return eval(compile(ast.Expression(expr), "_dtype_of", "eval"), globals=module.__dict__)
 def _return_type_of(node: ast.FunctionDef, module: types.ModuleType) -> type: return _dtype_of(node.returns, module)
 def _parameter_types_of(node: ast.FunctionDef, module: types.ModuleType) -> tuple[type, ...]: return tuple(_dtype_of(arg.annotation, module) for arg in node.args.args)
+
+def size_of(dtype: type) -> dtypes.USize: return ctypes.sizeof(_dtype_to_ctype(dtype))
+def addr_of(o: _T) -> dtypes.Pointer[_T]: return ctypes.addressof(typing.cast(typing.Any, o))
 
 @typing.overload
 def entry(maybe: typing.Callable[_P, _R], /) -> typing.Callable[_P, _R]: ...
@@ -54,6 +68,14 @@ def foreign(library: str, /, *, alt_name: str | None = None) -> typing.Callable[
 		f.restype, *f.argtypes = map(_dtype_to_ctype, (_return_type_of(node, module), *_parameter_types_of(node, module)))
 		return f
 	return wrapper
+
+@typing.overload
+def struct(maybe: _T, /) -> _T: ...
+@typing.overload
+def struct(maybe: None = None, /, *, packed: bool = False) -> typing.Callable[[_T], _T]: ...
+def struct(maybe: _T | None = None, /, *, packed: bool = False) -> _T | typing.Callable[[type], type]:
+	def wrapper(cls: type) -> type: return dtypes.Struct[cls.__name__, [(name, dtype) for name, dtype in cls.__annotations__.items()], packed]
+	return wrapper if maybe is None else wrapper(typing.cast(type, maybe))
 
 def find_entry(module: types.ModuleType) -> typing.Callable[..., typing.Any]:
 	for export in module.__dict__:
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from c4 import dtypes, entry, foreign, G, OSs, OS
+from c4 import dtypes, entry, foreign, G, OSs, OS, addr_of, size_of, struct
 
 if OS == OSs.WINDOWS:
 	# kernel32
@@ -17,12 +17,35 @@ if OS == OSs.WINDOWS:
 	HBRUSH = dtypes.Pointer[...]
 	HCURSOR = dtypes.Pointer[...]
 	HMONITOR = dtypes.Pointer[...]
+	WNDPROC = dtypes.Procedure[dtypes.SSize, HWND, dtypes.CUInt, dtypes.USize, dtypes.SSize]
+	@struct
+	class WNDCLASSEXW:
+		cbSize: dtypes.CUInt
+		style: dtypes.CUInt
+		lpfnWndProc: WNDPROC
+		cbClsExtra: dtypes.CInt
+		cbWndExtra: dtypes.CInt
+		hInstance: HINSTANCE
+		hIcon: HICON
+		hCursor: HCURSOR
+		hbrBackground: HBRUSH
+		lpszMenuName: dtypes.Pointer[dtypes.CWChar]
+		lpszClassName: dtypes.Pointer[dtypes.CWChar]
+		hIconSm: HICON
+
+	@foreign("user32")
+	def RegisterClassExW(wndclass: dtypes.Pointer[WNDCLASSEXW]) -> dtypes.CUShort: ...
 
 	platform_hinstance: HINSTANCE
 
 	@entry
 	def WinMainCRTStartup() -> dtypes.NoReturn:
 		G.platform_hinstance = GetModuleHandleW(None)
+
+		wndclass = WNDCLASSEXW()
+		wndclass.cbSize = size_of(WNDCLASSEXW)
+		RegisterClassExW(addr_of(wndclass))
+
 		ExitProcess(0)
 elif OS == OSs.DARWIN:
 	@foreign("System", alt_name="_exit")
