2e227ca9600508deb6ad10a0c3405e360991f0fc
Dawson Frakes <dawsonmfrakes@gmail.com>
2025-07-02 18:56:16 -0400

    webgl2 init

--- build.d
+++ build.d
@@ -47,9 +47,10 @@ immutable WASM_SOURCE = `
   <title>Outbreak Protocol</title>
 </head>
 <body>
+  <canvas></canvas>
   <script>
     let memory;
-    WebAssembly.instantiateStreaming(fetch("main.wasm"), {
+    WebAssembly.instantiateStreaming(fetch("OutbreakProtocol.wasm"), {
       env: {
         console_log: (len, ptr) => console.log(new TextDecoder().decode(new Uint8Array(memory.buffer, ptr, len))),
       },
@@ -57,6 +58,54 @@ immutable WASM_SOURCE = `
     .then(res => {
       memory = res.instance.exports.memory;
       res.instance.exports._start();
+
+      const canvas = document.getElementsByTagName("canvas")[0];
+      const gl = canvas.getContext("webgl2", {antialias: false});
+      const main_fbo = gl.createFramebuffer();
+      const main_fbo_color0 = gl.createRenderbuffer();
+      const main_fbo_depth = gl.createRenderbuffer();
+      gl.getExtension("EXT_color_buffer_float");
+
+      const main_fbo_sampled = gl.createFramebuffer();
+      const main_fbo_color0_sampled = gl.createRenderbuffer();
+
+      const fbo_samples = gl.getParameter(gl.SAMPLES);
+
+      gl.bindRenderbuffer(gl.RENDERBUFFER, main_fbo_color0);
+      gl.renderbufferStorageMultisample(gl.RENDERBUFFER, fbo_samples, gl.RGBA16F, canvas.width, canvas.height);
+
+      gl.bindRenderbuffer(gl.RENDERBUFFER, main_fbo_depth);
+      gl.renderbufferStorageMultisample(gl.RENDERBUFFER, fbo_samples, gl.DEPTH_COMPONENT32F, canvas.width, canvas.height);
+
+      gl.bindRenderbuffer(gl.RENDERBUFFER, main_fbo_color0_sampled);
+      gl.renderbufferStorage(gl.RENDERBUFFER, gl.RGBA16F, canvas.width, canvas.height);
+
+      gl.bindRenderbuffer(gl.RENDERBUFFER, null);
+
+      gl.bindFramebuffer(gl.FRAMEBUFFER, main_fbo);
+      gl.framebufferRenderbuffer(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.RENDERBUFFER, main_fbo_color0);
+      gl.framebufferRenderbuffer(gl.FRAMEBUFFER, gl.DEPTH_ATTACHMENT, gl.RENDERBUFFER, main_fbo_depth);
+      gl.bindFramebuffer(gl.FRAMEBUFFER, main_fbo_sampled);
+      gl.framebufferRenderbuffer(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.RENDERBUFFER, main_fbo_color0_sampled);
+      gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+
+      const frame = () => {
+        gl.bindFramebuffer(gl.FRAMEBUFFER, main_fbo);
+        gl.clearBufferfv(gl.COLOR, 0, [0.6, 0.2, 0.2, 1.0]);
+
+        gl.bindFramebuffer(gl.READ_FRAMEBUFFER, main_fbo);
+        gl.bindFramebuffer(gl.DRAW_FRAMEBUFFER, main_fbo_sampled);
+        gl.blitFramebuffer(0, 0, canvas.width, canvas.height, 0, 0, canvas.width, canvas.height, gl.COLOR_BUFFER_BIT, gl.NEAREST);
+
+        gl.bindFramebuffer(gl.READ_FRAMEBUFFER, main_fbo_sampled);
+        gl.bindFramebuffer(gl.DRAW_FRAMEBUFFER, null);
+        gl.blitFramebuffer(0, 0, canvas.width, canvas.height, 0, 0, canvas.width, canvas.height, gl.COLOR_BUFFER_BIT, gl.NEAREST);
+
+        gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+
+        requestAnimationFrame(frame);
+      };
+      requestAnimationFrame(frame);
     });
   </script>
 </body>
