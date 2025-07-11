b455e26e8714f6a9a37c3372adddd9a14b14dcf2
Dawson Frakes <dawsonmfrakes@gmail.com>
2025-07-10 22:09:04 -0400

    uhhh i really couldn't decide

--- platform/renderer.d
+++
@@ -1,9 +0,0 @@
-import basic;
-
-struct PlatformRenderer {
-  const(char)[] pretty_name;
-  void function() init;
-  void function() deinit;
-  void function() resize;
-  void function() present;
-}
