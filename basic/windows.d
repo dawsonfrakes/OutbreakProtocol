f28be324c3752cc8003e465abf76046cce81141c
Dawson Frakes <dawsonmfrakes@gmail.com>
2025-07-25 09:15:29 -0400

    progress

--- basic/windows.d
+++ basic/windows.d
@@ -2,6 +2,8 @@ module basic.windows;
 
 import basic;
 
+// Kernel32
+alias HRESULT = s32;
 struct HINSTANCE__;
 alias HINSTANCE = HINSTANCE__*;
 alias HMODULE = HINSTANCE;
@@ -11,9 +13,138 @@ alias PROC = extern(Windows) ssize function();
   HMODULE GetModuleHandleW(const(wchar)*);
   HMODULE LoadLibraryW(const(wchar)*);
   PROC GetProcAddress(HMODULE, const(char)*);
+  void Sleep(u32);
   noreturn ExitProcess(u32);
+}
+
+// User32
+enum CS_OWNDC = 0x0020;
+enum IDI_WARNING = cast(const(wchar)*) 32515;
+enum IDC_CROSS = cast(const(wchar)*) 32515;
+enum WS_MAXIMIZEBOX = 0x00010000;
+enum WS_MINIMIZEBOX = 0x00020000;
+enum WS_THICKFRAME = 0x00040000;
+enum WS_SYSMENU = 0x00080000;
+enum WS_CAPTION = 0x00C00000;
+enum WS_VISIBLE = 0x10000000;
+enum WS_OVERLAPPEDWINDOW = WS_CAPTION | WS_SYSMENU | WS_THICKFRAME | WS_MINIMIZEBOX | WS_MAXIMIZEBOX;
+enum CW_USEDEFAULT = 0x80000000;
+enum PM_REMOVE = 0x0001;
+enum WM_CREATE = 0x0001;
+enum WM_DESTROY = 0x0002;
+enum WM_SIZE = 0x0005;
+enum WM_PAINT = 0x000F;
+enum WM_QUIT = 0x0012;
+enum WM_ERASEBKGND = 0x0014;
+enum WM_ACTIVATEAPP = 0x001C;
+enum WM_SYSCOMMAND = 0x0112;
+enum SC_KEYMENU = 0xF100;
+
+struct HDC__;
+alias HDC = HDC__*;
+struct HWND__;
+alias HWND = HWND__*;
+struct HMENU__;
+alias HMENU = HMENU__*;
+struct HICON__;
+alias HICON = HICON__*;
+struct HBRUSH__;
+alias HBRUSH = HBRUSH__*;
+struct HCURSOR__;
+alias HCURSOR = HCURSOR__*;
+struct HMONITOR__;
+alias HMONITOR = HMONITOR__*;
+alias WNDPROC = extern(Windows) ssize function(HWND, u32, usize, ssize);
+struct POINT {
+  s32 x;
+  s32 y;
+}
+struct RECT {
+  s32 left;
+  s32 top;
+  s32 right;
+  s32 bottom;
+}
+struct WNDCLASSEXW {
+  u32 cbSize;
+  u32 style;
+  WNDPROC lpfnWndProc;
+  s32 cbClsExtra;
+  s32 cbWndExtra;
+  HINSTANCE hInstance;
+  HICON hIcon;
+  HCURSOR hCursor;
+  HBRUSH hbrBackground;
+  const(wchar)* lpszMenuName;
+  const(wchar)* lpszClassName;
+  HICON hIconSm;
+}
+struct MSG {
+  HWND hwnd;
+  u32 message;
+  usize wParam;
+  ssize lParam;
+  u32 time;
+  POINT pt;
+  u32 lPrivate;
 }
 
 @foreign("User32") extern(Windows) {
   s32 SetProcessDPIAware();
+  HICON LoadIconW(HINSTANCE, const(wchar)*);
+  HCURSOR LoadCursorW(HINSTANCE, const(wchar)*);
+  u16 RegisterClassExW(const(WNDCLASSEXW)*);
+  HWND CreateWindowExW(u32, const(wchar)*, const(wchar)*, u32, s32, s32, s32, s32, HWND, HMENU, HINSTANCE, void*);
+  s32 PeekMessageW(MSG*, HWND, u32, u32, u32);
+  s32 TranslateMessage(const(MSG)*);
+  ssize DispatchMessageW(const(MSG)*);
+  HDC GetDC(HWND);
+  ssize DefWindowProcW(HWND, u32, usize, ssize);
+  void PostQuitMessage(s32);
+}
+
+// Ws2_32
+enum WSADESCRIPTION_LEN = 256;
+enum WSASYS_STATUS_LEN = 128;
+
+struct WSADATA32 {
+	u16 wVersion;
+	u16 wHighVersion;
+	char[WSADESCRIPTION_LEN + 1] szDescription;
+	char[WSASYS_STATUS_LEN + 1] szSystemStatus;
+	u16 iMaxSockets;
+	u16 iMaxUdpDg;
+	char* lpVendorInfo;
+}
+struct WSADATA64 {
+	u16 wVersion;
+	u16 wHighVersion;
+	u16 iMaxSockets;
+	u16 iMaxUdpDg;
+	char* lpVendorInfo;
+	char[WSADESCRIPTION_LEN + 1] szDescription;
+	char[WSASYS_STATUS_LEN + 1] szSystemStatus;
+}
+version (Win32) alias WSADATA = WSADATA32;
+version (Win64) alias WSADATA = WSADATA64;
+
+@foreign("Ws2_32") extern(Windows) {
+  s32 WSAStartup(u16, WSADATA*);
+  s32 WSACleanup();
+}
+
+// Dwmapi
+enum DWMWA_USE_IMMERSIVE_DARK_MODE = 20;
+enum DWMWA_WINDOW_CORNER_PREFERENCE = 33;
+enum DWMWCP_DONOTROUND = 1;
+
+@foreign("Dwmapi") extern(Windows) {
+  HRESULT DwmSetWindowAttribute(HWND, u32, const(void)*, u32);
+}
+
+// Winmm
+enum TIMERR_NOERROR = 0;
+
+@foreign("Winmm") extern(Windows) {
+  u32 timeBeginPeriod(u32);
 }
--- platform/main_windows.d
+++ platform/main_windows.d
@@ -3,10 +3,15 @@ mixin(import_dynamic("basic.windows", attributes: ["__gshared"], except: ["Kerne
 
 __gshared {
   HINSTANCE platform_hinstance;
+  HWND platform_hwnd;
+  HDC platform_hdc;
 }
 
 extern(Windows) noreturn WinMainCRTStartup() {
   auto User32_dll = LoadLibraryW("USER32.DLL");
+  auto Ws2_32_dll = LoadLibraryW("WS2_32.DLL");
+  auto Dwmapi_dll = LoadLibraryW("DWMAPI.DLL");
+  auto Winmm_dll = LoadLibraryW("WINMM.DLL");
   static foreach (member; __traits(allMembers, basic.windows)) {
     static if (has_uda!(foreign, __traits(getMember, basic.windows, member)) &&
      !string_equal!(get_uda!(foreign, __traits(getMember, basic.windows, member)).library, "Kernel32"))
@@ -17,7 +22,63 @@ extern(Windows) noreturn WinMainCRTStartup() {
 
   platform_hinstance = GetModuleHandleW(null);
 
+  WSADATA wsadata = void;
+  bool networking_supported = WSAStartup && WSAStartup(0x202, &wsadata) == 0;
+
+  bool sleep_is_granular = timeBeginPeriod && timeBeginPeriod(1) == TIMERR_NOERROR;
+
   SetProcessDPIAware();
+  WNDCLASSEXW wndclass;
+  wndclass.cbSize = WNDCLASSEXW.sizeof;
+  wndclass.style = CS_OWNDC;
+  wndclass.lpfnWndProc = (hwnd, message, wParam, lParam) {
+    switch (message) {
+      case WM_CREATE:
+        platform_hwnd = hwnd;
+        platform_hdc = GetDC(hwnd);
+
+        if (DwmSetWindowAttribute) {
+          s32 dark_mode = true;
+          DwmSetWindowAttribute(hwnd, DWMWA_USE_IMMERSIVE_DARK_MODE, &dark_mode, dark_mode.sizeof);
+          s32 round_mode = DWMWCP_DONOTROUND;
+          DwmSetWindowAttribute(hwnd, DWMWA_WINDOW_CORNER_PREFERENCE, &round_mode, round_mode.sizeof);
+        }
+        return 0;
+      case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+      default:
+        return DefWindowProcW(hwnd, message, wParam, lParam);
+    }
+  };
+  wndclass.hInstance = platform_hinstance;
+  wndclass.hIcon = LoadIconW(null, IDI_WARNING);
+  wndclass.hCursor = LoadCursorW(null, IDC_CROSS);
+  wndclass.lpszClassName = "A";
+  RegisterClassExW(&wndclass);
+  CreateWindowExW(0, wndclass.lpszClassName, "Outbreak Protocol",
+    WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+    CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+    null, null, platform_hinstance, null);
+
+  main_loop: while (true) {
+    MSG msg = void;
+    while (PeekMessageW(&msg, null, 0, 0, PM_REMOVE)) {
+      TranslateMessage(&msg);
+      with (msg) switch (message) {
+        case WM_QUIT:
+          break main_loop;
+        default:
+          DispatchMessageW(&msg);
+      }
+    }
+
+    if (sleep_is_granular) {
+      Sleep(1);
+    }
+  }
+
+  if (networking_supported) WSACleanup();
 
   ExitProcess(0);
 }
